5513e3afa56c904170a178baccd2e6cafcbd6a5c
JACK <luckyjack85@gmail.com>
2017-06-07 18:18:34 +0800

    hospital done

---
+++ detect.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+grep "Shilin District" hospital.out | wc -l
+grep "Datong District" hospital.out | wc -l
+grep "Zhongshan District" hospital.out | wc -l
+grep "Songshan District" hospital.out | wc -l
+grep "Da’an District" hospital.out | wc -l
+grep "Wanhua District" hospital.out | wc -l
+grep "Xinyi District" hospital.out | wc -l
+grep "Shilin District" hospital.out | wc -l
+grep "Beitou District" hospital.out | wc -l
+grep "Neihu District" hospital.out | wc -l
+grep "Nangang District" hospital.out | wc -l
+grep "Wenshan District" hospital.out | wc -l
--- googlemap.js
+++ googlemap.js
@@ -1,9 +1,9 @@
 var hospital = require('./hospital.json')
-
+var test = require('./hostest.json')
 var googleMapsClient = require('@google/maps').createClient({
   key: 'AIzaSyClG1COBS6izq7X-_rTqMjp-N01GGNaMSA'
 });
-
+var sleep = require('sleep')
 
 // function find(target) {
 //   hospital.map(getaddress(v.addr))
@@ -11,12 +11,16 @@ var googleMapsClient = require('@google/maps').createClient({
 
 function getaddress(value) {
    googleMapsClient.geocode({
-    address: '台北市仁愛路4段112巷12號'
+    address: value
   }, function(err, response) {
     if (!err) {
-      console.log(response.json.results[0].formatted_address)
+      console.log(response.json.results[0].formatted_address,",")
     }
   });
 }
 
-getaddress("testing")
+test.map((v) =>{
+  getaddress(v.addr)
+  sleep.msleep(100)
+})
+// getaddress('台北市仁愛路4段112巷12號')
